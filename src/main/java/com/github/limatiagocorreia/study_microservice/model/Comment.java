59d1f0d971a53fa35ad3167d0759ba83c56a9075
Logon Aluno <logonrmlocal@fiap.com.br>
2022-03-30 19:47:50 -0300

    adicionando classe comment

---
+++ src/main/java/com/github/limatiagocorreia/study_microservice/model/Comment.java
@@ -0,0 +1,31 @@
+package com.github.limatiagocorreia.study_microservice.model;
+
+public class Comment {
+    private Long id;
+    private String body;
+    private Long postId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+}
